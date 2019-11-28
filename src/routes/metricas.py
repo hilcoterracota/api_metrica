507034dd1f4914ddf536dc409f8967c3e90fb8a2
snwnst <nestor-mh@outlook.com>
2019-11-28 10:54:40 -0600

    horas

--- src/routes/metricas.py
+++ src/routes/metricas.py
@@ -15,25 +15,24 @@ def get_blueprint():
 @METRICAS_API.route('/metricas/metricasclean/', methods=['GET'])
 def get_metricas_clean():
     mycol = mydb["info_pc_historico"]
+    catalogos = list(mydb["catalogos"].find())[0]
     data = []
     for item in mycol.find():
         historico = []
         tiempo_uso_global = datetime.strptime('00:00:00', '%H:%M:%S')
+        print(item["usuario"])
         for proseso in item["historico"]:
-            if str(proseso["fecha"]) == str(date.today()):
-                historico.append(proseso)
-                time_sub_aux = proseso["tiempoTotal"].split(":")
-                sub_minutos = (int(time_sub_aux[0]))+int(time_sub_aux[1])
-                sub_seconds_aux = (sub_minutos)+int(time_sub_aux[2])
-                tiempo_uso_global = tiempo_uso_global + timedelta(seconds=int(sub_seconds_aux))
-        if not historico:
-            print("pc apagada")
-        else:
-            data.append({
-                "usuario":item["usuario"],
-                "historico":sorted(historico, key=lambda element: element['usoMemoria'],reverse=True),
-                "tiempoTotal":str(tiempo_uso_global.strftime("%H:%M:%S"))
-            })
+            if str(proseso["fecha"]) == str(date.today()) and str(proseso["nombre"]) not in catalogos["info_pc_exclude"]:
+                historico.append(proseso)          
+                h2 = str(proseso["tiempoTotal"]).split(":")
+                h2_a = ((int(h2[0]))+int(h2[1])*60)+int(h2[2])
+                tiempo_uso_global = tiempo_uso_global + timedelta(seconds=int(h2_a))
+        print(str(tiempo_uso_global.strftime("%H:%M:%S")))   
+        data.append({
+            "usuario":item["usuario"],
+            "historico":sorted(historico, key=lambda element: element['usoMemoria'],reverse=True),
+            "tiempoTotal":str(tiempo_uso_global.strftime("%H:%M:%S"))
+        })
 
     return dumps(data), 200
 
