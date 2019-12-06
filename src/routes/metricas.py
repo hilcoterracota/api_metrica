3b606b4cba2dad2d1a6b9a9c2f7007e9bf98b1b5
snwnst <nestor-mh@outlook.com>
2019-12-06 11:57:46 -0600

    nav

--- src/routes/metricas.py
+++ src/routes/metricas.py
@@ -180,8 +180,7 @@ def get_metricas_history_pie():
     for app in catalogos["info_pc_navegadores"]: 
         x = dataset[dataset["nombre"] == app]
         x = x[dataset["fecha"] == hoy] 
-        for y in x["tiempoTotal"].tolist():
-            t_navegadores.append(sum_time_array(t_navegadores,True))
+        t_navegadores.append(sum_time_array(t_navegadores,True))
 
     ## APLICATIVOS
     data_aplicativos = []
